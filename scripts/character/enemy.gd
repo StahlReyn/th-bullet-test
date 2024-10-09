0460296414d6aac31c61c1e42f01f1da6e8fdf86
StahlReyn <reynstahl@gmail.com>
2024-10-08 22:16:07 -0400

    enemy support animation as path

--- scripts/character/enemy.gd
+++ scripts/character/enemy.gd
@@ -1,6 +1,16 @@
 class_name Enemy
 extends Character
 
+@export var animation_player : AnimationPlayer
+
+func _ready() -> void:
+	super()
+	if animation_player:
+		animation_player.play("default")
+
+func _process(delta: float) -> void:
+	super(delta)
+
 func do_death():
 	print("DEATH!")
 	if audio_death:
