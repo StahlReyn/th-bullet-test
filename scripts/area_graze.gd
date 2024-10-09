0720fa258176d77b2c544d710d9b20b0b7d4a9ea
StahlReyn <reynstahl@gmail.com>
2024-10-08 21:12:37 -0400

    focus transition shows dot

--- scripts/area_graze.gd
+++ scripts/area_graze.gd
@@ -1,7 +1,10 @@
 class_name AreaGraze
 extends Area2D
 
+@onready var focus_sprite : Sprite2D = $FocusDot
 @onready var graze_sprite : Sprite2D = $GrazeCircle
+
+var focus_transition_speed : float = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +14,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	graze_sprite.rotation += delta
+	if Input.is_action_pressed("focus"):
+		graze_sprite.scale = lerp(graze_sprite.scale, Vector2(2,2), delta * focus_transition_speed)
+		graze_sprite.set_modulate(lerp(graze_sprite.get_modulate(), Color(1,1,1,0.5), delta * focus_transition_speed))
+		focus_sprite.set_modulate(lerp(focus_sprite.get_modulate(), Color(1,1,1,1), delta * focus_transition_speed))
+	else:
+		graze_sprite.scale = lerp(graze_sprite.scale, Vector2(2.5,2.5), delta * focus_transition_speed)
+		graze_sprite.set_modulate(lerp(graze_sprite.get_modulate(), Color(1,1,1,0), delta * focus_transition_speed))
+		focus_sprite.set_modulate(lerp(focus_sprite.get_modulate(), Color(1,1,1,0), delta * focus_transition_speed))
+		
 	pass
