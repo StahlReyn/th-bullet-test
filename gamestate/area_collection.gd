a90bd9bd217378d349d1a5a1038bbd6524afd214
StahlReyn <reynstahl@gmail.com>
2024-10-10 13:18:30 -0400

    item collection line + Nearest neighbor default

--- gamestate/area_collection.gd
+++ gamestate/area_collection.gd
@@ -1,6 +1,20 @@
 class_name AreaCollection
 extends Area2D
 
+var appear_time : float = 0
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	appear_time -= delta
+	if appear_time > 0:
+		visible = true
+		var color = self.get_modulate()
+		color.a = -cos(appear_time * 2) * 0.5 + 0.5
+		set_modulate(color)
+	else:
+		visible = false
+
 func _on_area_entered(area: Area2D) -> void:
 	print("Collection Enter")
 	for item in GameUtils.get_point_items(self):
@@ -11,3 +25,8 @@ func _on_area_exited(area: Area2D) -> void:
 	for item in GameUtils.get_point_items(self):
 		item.magnet_target = null
 		item.maximum_collect = false
+
+func _on_gameview_game_start() -> void:
+	appear_time = 7.0
+	
+	
--- gamestate/hud/gameview.gd
+++ gamestate/hud/gameview.gd
@@ -1,11 +1,14 @@
 class_name GameView
 extends Node2D
 
+signal game_start
+
 var game_time : float = 0.0
 var score : int = 0
 var graze_count : int = 0
 
 func _ready() -> void:
+	game_start.emit()
 	pass
 
 func _process(delta: float) -> void:
