ba03560a1ec0207ca0b2202a508414a28f1a16c1
StahlReyn <reynstahl@gmail.com>
2024-10-12 20:01:56 -0400

    move file to script, menu scene switch, Scene Handler

--- scripts/scene_handler.gd
+++ scripts/scene_handler.gd
@@ -2,6 +2,8 @@ extends Node
 
 var current_scene = null
 
+var scene_game = "res://scripts/gamestate/gamemain.tscn"
+
 func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
