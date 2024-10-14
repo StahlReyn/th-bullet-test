1db0a3e3edb426d88a18e3343971992f1e22facd
StahlReyn <reynstahl@gmail.com>
2024-10-14 15:24:26 -0400

    font + basic HUD

---
+++ scripts/gamehud/life_container.gd
@@ -0,0 +1,11 @@
+extends BoxContainer
+
+@export var text_main : String = "Life"
+
+@onready var label_main : Label = $SymbolContainer/LabelMain
+
+func _ready() -> void:
+	label_main.text = text_main
+
+func _process(delta: float) -> void:
+	pass
--- scripts/globals/game_variables.gd
+++ scripts/globals/game_variables.gd
@@ -3,13 +3,14 @@ extends Node
 var game_time : float = 0.0
 var score : int = 0
 var graze : int = 0
+var point_value : int = 10000
 
 var lives : int = 3
 var bombs : int = 3
 var power : int = 0 ##Powers are in integer for simplicity, display divides by 100
 
 static var power_min : int = 0
-static var power_max : int = 500
+static var power_max : int = 400
 
 func reset_variables() -> void:
 	game_time = 0.0
@@ -18,6 +19,7 @@ func reset_variables() -> void:
 	lives = 3
 	bombs = 3
 	power = 0
+	point_value = 10000
 
 func add_score(value: int) -> void:
 	score += value
@@ -44,3 +46,35 @@ func add_power(value:int = 1) -> void:
 func lose_power(value:int = 1) -> void:
 	power += value
 	power = clamp(power, power_min, power_max)
+
+func get_score_display():
+	return thousands_sep(score)
+
+func get_power_display():
+	return two_decimal_int(power) + "/" + two_decimal_int(power_max)
+
+func get_point_value_display():
+	return thousands_sep(point_value)
+
+func get_graze_display():
+	return thousands_sep(graze)
+
+static func two_decimal_int(number : int) -> String:
+	return "%.2f" % (float(number) / 100)
+	
+static func thousands_sep(number, prefix='') -> String:
+	number = int(number)
+	var neg = false
+	if number < 0:
+		number = -number
+		neg = true
+	var string = str(number)
+	var mod = string.length() % 3
+	var res = ""
+	for i in range(0, string.length()):
+		if i != 0 && i % 3 == mod:
+			res += ","
+		res += string[i]
+	if neg: res = '-'+prefix+res
+	else: res = prefix+res
+	return res
