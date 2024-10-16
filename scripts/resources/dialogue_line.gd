6187127929f57c26a3b7f5f65ba4d3bd5a593fbc
StahlReyn <reynstahl@gmail.com>
2024-10-16 12:46:25 -0400

    csv file for system

--- scripts/resources/dialogue_line.gd
+++ scripts/resources/dialogue_line.gd
@@ -1,6 +1,6 @@
 class_name DialogueLine
 extends Resource
 
-@export var name : String
-@export_file("*.png") var portrait : String
-@export_multiline  var text : String
+@export var name : String ## Name of speaker in case needed, use CSV keys
+@export_file("*.png") var portrait : String ## Portrait shown
+@export var text : String ## Dialogue content, use CSV Keys
