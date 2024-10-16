d6b18399c7811f52fa4693389c7e5eb1c86458d1
StahlReyn <reynstahl@gmail.com>
2024-10-16 13:38:08 -0400

    portrait set atlas

---
+++ scripts/dialogue_displayer/portrait_set.gd
@@ -0,0 +1,12 @@
+class_name PortraitSet
+extends Node2D
+
+
+@export var sprite_body : AnimatedSprite2D ## Sprite set for body, larger part
+@export var sprite_face : AnimatedSprite2D ## Sprite set for face, smaller part
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
